8c3d1b29dc56a886462fcb1a71f6de3b053161fb
florentinc <florentina.cociota@temasoft.com>
2016-05-12 11:02:59 +0300

    Add Ui controls programmatically

--- Store/Store/MainWindow.xaml.cs
+++ Store/Store/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using StoreCore.Repository;
 
 namespace Store
@@ -28,35 +32,55 @@ namespace Store
 
         private void OuterListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {            
+            StackPanel.Children.Clear();
             var item = ProductsList.SelectedItem;
             if (item != null)
             {
                 var productRepository = new ProductRepository(@"Repository\test.txt", "Product");
                 var products = productRepository.GetByCategory(item.ToString());
-                ProductImage.Source = productRepository.GetImageByID(4); 
-                                             
+                foreach (var product in products)
+                {
+                    var grid = CreateGridDinamicaly(StackPanel);
+                    CreateButton("Buy Product","BuyProduct",grid);
+                    AddImage(productRepository.GetImageByID(product.Id), grid);
+                    CreateLabelNameDynamically(grid,product.Name);
+                    CreateLabelDescriptionDynamically(grid,"This should be a product description!");
+                }                                                           
             }
-            var product1 = new Product { Category = "Food", Name = "bread", Price = 10, Stock = 2, Image = @"\Repository\bread.jpg" };
-            NameLabel.Content = product1.Name;
-            DescriptionLabel.Content = "This is a good bread";
-            PriceLabel.Content = product1.Price + " lei";
-            SetStockMessage(product1);
         }
 
-        private void SetStockMessage(Product product1)
+        private void SetStockMessage(int stock, Label label)
         {
-            if (product1.Stock == 0)
+            if (stock == 0)
             {
-                StockLabel.Content = "Unavailable stock";
+                label.Content = "Unavailable stock";
             }
-            else if (product1.Stock <= 5)
+            else if (stock <= 5)
             {
-                StockLabel.Content = "Few in stock";
+                label.Content = "Few in stock";
             }
             else
             {
-                StockLabel.Content = "In stock";
+                label.Content = "In stock";
             }
+        }
+        private Grid CreateGridDinamicaly(StackPanel stackPanel)
+        {
+            var grid = new Grid();
+
+            var gridCol1 = new ColumnDefinition();            
+            var gridCol2 = new ColumnDefinition();
+            var gridCol3 = new ColumnDefinition();
+            grid.ColumnDefinitions.Add(gridCol1);
+            grid.ColumnDefinitions.Add(gridCol2);
+            grid.ColumnDefinitions.Add(gridCol3);
+
+            var gridRow1 = new RowDefinition();         
+            grid.RowDefinitions.Add(gridRow1);
+           
+            stackPanel.Children.Add(grid);
+
+            return grid;
         }
 
         private void BuyButton_Click(object sender, RoutedEventArgs e)
@@ -65,6 +89,77 @@ namespace Store
             productRepository.UpdateStock(6);
             MessageBox.Show("The product was added to the cart");
         }
+
+        private void CreateButton(string content, string name, Grid grid)
+        {
+            var newButton = new Button
+            {
+                Content = content,
+                Name = name
+            };
+
+            newButton.Click += (BuyButton_Click);
+            newButton.Width = 100;
+            newButton.Height = 30;   
+            Grid.SetColumn(newButton,2);
+            Grid.SetRow(newButton,0);     
+            grid.Children.Add(newButton);
+        }
+
+        private void AddImage(BitmapImage image, Grid grid)
+        {
+            var i = new Image
+            {
+                Source = image,
+                Stretch = Stretch.Uniform,
+                Height = 80,
+                Width = 80
+            };
+
+            Grid.SetColumn(i,0);
+            Grid.SetRow(i,0);
+            grid.Children.Add(i);
+        }
+
+        private void CreateLabelNameDynamically(Grid grid, string name)
+        {
+            var dynamicLabel = new Label
+            {
+                Name = name,
+                Width = 240,
+                Height = 50,
+                Foreground = new SolidColorBrush(Colors.Black),
+                Content = name,
+                FontWeight = FontWeights.Bold,
+                FontSize = 16
+            };
+
+            Grid.SetColumn(dynamicLabel,1);
+            Grid.SetRow(dynamicLabel,0);
+            grid.Children.Add(dynamicLabel);
+        }
+
+        private void CreateLabelDescriptionDynamically(Grid grid, string content)
+        {
+            var dynamicLabel = new Label
+            {
+                Name = "DescriptionLabel",
+                Width = 240,
+                Height = 50,
+                Foreground = new SolidColorBrush(Colors.Black),
+                Content = Environment.NewLine + content,
+                FontSize = 14,                            
+            };
+
+            Grid.SetColumn(dynamicLabel, 1);
+            Grid.SetRow(dynamicLabel, 0);
+            TextBlock txtBlock = new TextBlock();
+            txtBlock.TextWrapping = TextWrapping.Wrap;
+            dynamicLabel.Content = txtBlock;
+            grid.Children.Add(dynamicLabel);
+            
+        }
     }
+    
 }
 
--- Store/StoreCore.Repository.Tests/ProductRepository.Tests.cs
+++ Store/StoreCore.Repository.Tests/ProductRepository.Tests.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.IO;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using StoreCore.Repository;
+using Should;
 
 namespace StoreCore.Tests
 {
@@ -17,7 +20,55 @@ namespace StoreCore.Tests
                 Price = 10,
                 Stock = 4
             });
-            
+            Assert.IsTrue(File.Exists(@"Repository\Products.txt"));
+        }
+
+        [TestMethod]
+        public void ShouldGetAllProduct()
+        {
+            var productRepository = new ProductRepository(@"Repository\Products.txt", "Product");
+            var actualResult = productRepository.GetAll();
+            Assert.AreEqual(3, actualResult.Count());
+        }
+
+        [TestMethod]
+        public void ShouldGetAProductById()
+        {
+            var productRepository = new ProductRepository(@"Repository\Products.txt", "Product");
+            var actualResult = productRepository.GetById(2);
+            actualResult.ShouldNotBeNull();
+        }
+
+        [TestMethod]
+        public void ShouldDeleteAProduct()
+        {
+            var productRepository = new ProductRepository(@"Repository\Products.txt", "Product");
+            productRepository.Delete(2);
+            Assert.AreEqual(2,productRepository.GetAll().Count());
+        }
+
+        [TestMethod]
+        public void ShouldUpdateAProduct()
+        {
+            var productRepository = new ProductRepository(@"Repository\Products.txt", "Product");
+            productRepository.Update(2,new Product
+            {
+                Category = "Fruits",
+                Name = "Appel",
+                Price = 12,
+                Stock = 3
+            });
+            var products = productRepository.GetAll().ToArray();
+            Assert.AreEqual("Appel",products[1].Name);
+        }
+
+        [TestMethod]
+        public void ShouldUpdateTheStockForProduct()
+        {
+            var productRepository = new ProductRepository(@"Repository\Products.txt", "Product");
+            productRepository.UpdateStock(2);
+            var products = productRepository.GetAll().ToArray();
+            Assert.AreEqual(2, products[1].Stock);
         }
     }
 }
